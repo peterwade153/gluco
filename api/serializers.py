29e7369244469b9124c6e99f3bc2c476a32a4ad8
peterwade153 <peterwade153@gmail.com>
2022-07-04 16:59:44 +0300

    update

--- api/serializers.py
+++ api/serializers.py
@@ -10,7 +10,6 @@ class GlucoseLevelSerilizer(serializers.ModelSerializer):
         read_only_fields = ['id', 'gerätezeitstempel']
         fields = read_only_fields + ['user_id', 'seriennummer', 'aufzeichnungstyp','glukosewert_verlauf','glukose_scan']
 
-        extra_kwargs = {'user_id': {'required': True,'allow_blank': False}}
         extra_kwargs = {'seriennummer': {'required': True}}
         extra_kwargs = {'aufzeichnungstyp': {'required': True}}
         extra_kwargs = {'glukosewert_verlauf': {'required': True}}
