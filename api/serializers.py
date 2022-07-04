c2c5b8642de28532fab8e2c777c4208b09193c9f
peterwade153 <peterwade153@gmail.com>
2022-07-04 16:58:58 +0300

    Validate fields on create

--- api/serializers.py
+++ api/serializers.py
@@ -10,6 +10,13 @@ class GlucoseLevelSerilizer(serializers.ModelSerializer):
         read_only_fields = ['id', 'gerätezeitstempel']
         fields = read_only_fields + ['user_id', 'seriennummer', 'aufzeichnungstyp','glukosewert_verlauf','glukose_scan']
 
+        extra_kwargs = {'user_id': {'required': True,'allow_blank': False}}
+        extra_kwargs = {'seriennummer': {'required': True}}
+        extra_kwargs = {'aufzeichnungstyp': {'required': True}}
+        extra_kwargs = {'glukosewert_verlauf': {'required': True}}
+        extra_kwargs = {'glukose_scan': {'required': True}}
+
+
     def create(self, validated_data):
         reading = GlucoseLevel.objects.create(**validated_data)
         return reading
