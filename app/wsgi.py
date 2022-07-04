b612dc7e5d18d1968baed223bd08c151e135c3c8
peterwade153 <peterwade153@gmail.com>
2022-07-04 15:32:37 +0300

    upddate setings wsgi

--- app/wsgi.py
+++ app/wsgi.py
@@ -11,6 +11,11 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
+from utils.read_envs import read_env_file
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
+if 'ENV_VAR_FILE' in os.environ:
+    file_name = os.getenv('ENV_VAR_FILE')
+    read_env_file(file_name)
 
 application = get_wsgi_application()
